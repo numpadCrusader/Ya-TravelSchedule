04d96e687fab6512a26257cb4115db2bef41ea84
numpadCrusader <103027096+numpadCrusader@users.noreply.github.com>
2025-07-27 23:56:16 +0500

    initial commit

---
+++ TravelSchedule/TravelScheduleApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelScheduleApp.swift
+//  TravelSchedule
+//
+//  Created by Nikita Khon on 27.07.2025.
+//
+
+import SwiftUI
+
+@main
+struct TravelScheduleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
