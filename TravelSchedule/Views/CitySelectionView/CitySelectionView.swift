f6810b35751c70f702f1e0f60c5b0d6c7c7ffe84
numpadCrusader <103027096+numpadCrusader@users.noreply.github.com>
2025-10-17 22:12:42 +0500

    feat: create TextChevronRow

--- TravelSchedule/Views/CitySelectionView/CitySelectionView.swift
+++ TravelSchedule/Views/CitySelectionView/CitySelectionView.swift
@@ -11,12 +11,25 @@ struct CitySelectionView: View {
     @Environment(\.dismiss) private var dismiss
     @State private var searchQuery = ""
     
+    let fruits = [
+        "Apple", "Banana", "Orange", "Mango",
+        "Apple", "Banana", "Orange", "Mango",
+        "Apple", "Banana", "Orange", "Mango",
+        "Apple", "Banana", "Orange", "Mango",
+        "Apple", "Banana", "Orange", "Mango"
+    ]
+    
     var body: some View {
         VStack {
             SearchBar(text: $searchQuery, prompt: "Введите запрос")
-            Spacer()
-            Text(searchQuery)
-            Spacer()
+            
+            List(fruits, id: \.self) { fruit in
+                TextChevronRow(text: fruit)
+                    .listRowSeparator(.hidden)
+                    .listRowInsets(.init(top: 0, leading: 16, bottom: 0, trailing: 16))
+            }
+            .listStyle(.plain)
+            .padding(.top, 16)
         }
         .navigationTitle("Выбор города")
         .navigationBarTitleDisplayMode(.inline)
---
+++ TravelSchedule/Views/CitySelectionView/Subviews/TextChevronRow.swift
@@ -0,0 +1,29 @@
+//
+//  TextChevronRowv.swift
+//  TravelSchedule
+//
+//  Created by Nikita Khon on 17.10.2025.
+//
+
+import SwiftUI
+
+struct TextChevronRow: View {
+    let text: String
+    
+    var body: some View {
+        HStack {
+            Text(text)
+                .font(.system(size: 17, weight: .regular))
+                .foregroundStyle(.ypBlack)
+            
+            Spacer()
+            
+            Image(.icChevronRight24Px)
+        }
+        .frame(height: 60)
+    }
+}
+
+#Preview {
+    TextChevronRow(text: "Test")
+}
